b0feaad218b71d1317b2a829d8a31f978c5e5c4e
Andreas Sousa <devandreas96@gmail.com>
2025-04-29 21:50:59 -0300

    .

---
+++ index.js
@@ -0,0 +1,24 @@
+// let tituloAba = document.head.querySelector('title')
+// let input = document.querySelector('input')
+// let botao = document.querySelector('button')
+
+// botao.addEventListener('click', trocarNomedaAba)
+
+
+// function trocarNomedaAba(){
+//     let novoNome = input.value
+//     tituloAba.innerHTML = novoNome
+//     input.value = ""
+// }
+
+
+let number = 10
+
+for(let i = 1; i <= number; i++){
+    if(i % 2 == 0){
+        console.log(`O número ${i} é par!`)
+    } else {
+    console.log(`O número ${i} é impar!`)
+        
+    }
+}
